a0158dc440b71203f7dd49d8ee453c129f9e300a
Amruta Ghotankar <amruta.g@cursivetech.net>
2021-12-13 23:54:56 +0530

    first commit

---
+++ Game.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Participant{
+    Integer id;
+    String name;
+    Participant(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class LeaderBoardEntry {
+    Participant participant;
+    Integer level;
+    Integer score;
+
+    LeaderBoardEntry(Participant p, Integer level, Integer Score){
+        this.participant = p;
+        this.level = level;
+        this.score = score;
+        System.out.println("Level " + level + " score for " + p.name + " is " + Score);
+    }
+}
+
+class LeaderBoard {
+    List<LeaderBoardEntry> leaderBoardEntry = new ArrayList<LeaderBoardEntry>();
+    public void addEntry(Participant participant,Integer level, Integer score){
+        leaderBoardEntry.add(new LeaderBoardEntry(participant, level, score));
+    }
+}
+
+class Game{
+
+    Random rn = new Random();
+    List<Participant> participants = new ArrayList<Participant>();
+    LeaderBoard leaderBoard = new LeaderBoard();
+
+    private void init(){
+        Participant p1 = new Participant(1, "Sara");
+        Participant p2 = new Participant(2, "Rohit");
+        Participant p3 = new Participant(3, "Rahul");
+
+        participants.add(p1);
+        participants.add(p2);
+        participants.add(p3);
+
+        System.out.println("Game initialized with " + participants.size() + " participants");
+    }
+
+    private void playLevel(int level){
+        participants.forEach(participant -> leaderBoard.addEntry(participant, level, rn.nextInt(100)));
+    }
+
+    // Write code to identify and announce winner's name and score
+    private void declareWinner(){
+       
+
+       System.out.println(".... and the WINNER is " + " with score " );
+    }
+
+
+    public static void main(String args[]) {
+        System.out.println("Game Begins");
+        Game game = new Game();
+        game.init();
+        game.playLevel(1);
+        game.declareWinner();
+    }
+}
